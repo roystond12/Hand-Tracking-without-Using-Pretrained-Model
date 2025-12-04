8b34f2435dc544f7e751369f5ca9bcb2af4a4ec7
roystond12 <165377079+roystond12@users.noreply.github.com>
2025-12-04 13:38:17 +0530

    added virtual object

--- camera.py
+++ camera.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import time
 
+x1, y1, x2, y2 = 200, 100, 440, 380
+
 def camera():
 
     cap = cv2.VideoCapture(0)
@@ -13,7 +15,8 @@ def camera():
             break
         cv2.putText(img,f"FPS {1 / (ctime - p_time)}",(450,30),cv2.FONT_HERSHEY_SIMPLEX,1,(225,0,0))
         p_time = ctime
-
+        cv2.rectangle(img,(x1,y1),(x2,y2),(225,0,0))
+        cv2.putText(img, "SAFE",(20,40),cv2.FONT_HERSHEY_PLAIN,1,(255, 255, 255),1)
         cv2.imshow("img",img)
         key = cv2.waitKey(1)
         if key == ord('q'):
