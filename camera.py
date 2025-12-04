e2486996127187611cebc4bd5b54f621df153075
roystond12 <165377079+roystond12@users.noreply.github.com>
2025-12-04 23:58:21 +0530

    skin masking done

--- camera.py
+++ camera.py
@@ -1,27 +1,38 @@
 import cv2
 import numpy as np
 import time
+from segment import segmentation
 
 x1, y1, x2, y2 = 200, 100, 440, 380
 
 def camera():
-
+    frame = []
+    median = 0
     cap = cv2.VideoCapture(0)
-    p_time = 0
+    p_time = count = 0 
     while True:
         sucess,img = cap.read()
+        if count < 30:
+            frame.append(img)
+            count += 1
+        else:
+            median = np.median(frame,axis = 0).astype(dtype=np.uint8)
+            img = cv2.absdiff(img,median)
+        
         ctime = time.time()
         if not sucess or img is None:
             break
-        cv2.putText(img,f"FPS {1 / (ctime - p_time)}",(450,30),cv2.FONT_HERSHEY_SIMPLEX,1,(225,0,0))
+        cv2.putText(img,f"FPS {1 / (ctime - p_time)}",(450,30),cv2.FONT_HERSHEY_PLAIN,2,(225,0,0))
         p_time = ctime
         cv2.rectangle(img,(x1,y1),(x2,y2),(225,0,0))
         cv2.putText(img, "SAFE",(20,40),cv2.FONT_HERSHEY_PLAIN,1,(255, 255, 255),1)
-        cv2.imshow("img",img)
+        if np.any(median != 0):
+            segmentation(img)
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
-
+    
     cap.release()
     cv2.destroyAllWindows()
+
 
---
+++ segment.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+def segmentation(img):
+    kernel = np.ones((3,3),np.uint8)
+    img = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
+
+    lower = np.array([100, 50, 50]) 
+    upper = np.array([140, 255, 255])
+
+    mask = cv2.inRange(img,lower,upper)
+    mask = cv2.dilate(mask,kernel,iterations = 4)
+    img2 = cv2.GaussianBlur(mask,(5,5),0)
+
+    opening = cv2.morphologyEx(img2,cv2.MORPH_OPEN,kernel)
+    closing = cv2.morphologyEx(opening,cv2.MORPH_CLOSE,kernel)
+    cv2.imshow("img",closing)
+
